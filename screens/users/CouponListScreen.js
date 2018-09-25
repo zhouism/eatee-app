0be60c72d47f833ebb3c43e6f8c7ec57fb014c0c
veilance <christopher.tam@alumni.ubc.ca>
2018-09-24 19:28:44 -0700

    CouponListScreen can now pull from the database and render data

--- screens/users/CouponListScreen.js
+++ screens/users/CouponListScreen.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { Text, View, Button } from "react-native";
+import { Text, View, Button, FlatList } from "react-native";
+import axios from 'axios';
 
 export default class CouponListScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+    };
+  }
+
   static navigationOptions = {
     title: 'Your Food Options',
   };
 
+  componentDidMount(){
+    axios.get('http://192.168.88.173:3001/api/users/1/coupon_list')
+      .then(response => {
+        const datas = response.data
+        console.log(datas);
+        this.setState({
+          data: datas
+        })
+      })
+      .catch((error) =>  {
+        console.log(error);
+      });
+  }
+
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
     return (
       <View>
-        <Text>SHOW THE USER THEIR COUPONS</Text>
+      <FlatList
+        data={this.state.data}
+        renderItem={({item}) => <Text>{item.name}</Text>} 
+      />
       </View>
     )
   }
