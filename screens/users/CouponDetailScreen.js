7f5c3951bce96df816e0ab68f27809136b2a7c75
Hoa Hoang <6288799+tienhoah@users.noreply.github.com>
2018-09-26 16:45:17 -0700

    set up CouponDetailScreen.

---
+++ screens/users/CouponDetailScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class CouponDetailScreen extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+export default CouponDetailScreen;
