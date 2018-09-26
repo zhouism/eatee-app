4466b81c6640b3f07f2d9babe0fe55c909af2fbb
veilance <christopher.tam@alumni.ubc.ca>
2018-09-25 19:53:38 -0700

    added imagepicker from expo to CreateCouponBatchScreen

--- screens/restaurants/CouponBatchesScreen.js
+++ screens/restaurants/CouponBatchesScreen.js
@@ -1,24 +1,41 @@
-import React, { Component } from "react";
-import { Text, View, Image, Button, StyleSheet } from "react-native";
+import React from 'react';
+import { Text, View, Button, FlatList } from "react-native";
+import axios from 'axios';
 
-export default class CouponBatchesScreen extends Component {
+export default class CouponBatchesScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+    };
+  }
+
   static navigationOptions = {
-    title: 'CouponBatchesScreen!',
+    title: 'Your Coupon Ads',
   };
+
+  componentDidMount(){
+    axios.get('http://192.168.88.173:3001/api/restaurants/2/coupon_batches')
+      .then(response => {
+        const datas = response.data
+        console.log(datas);
+        this.setState({
+          data: datas
+        })
+      })
+      .catch((error) =>  {
+        console.log(error);
+      });
+  }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello</Text>
+      <View>
+      <FlatList
+        data={this.state.data}
+        renderItem={({item}) => <Text>{item.name} {item.impression}</Text>} 
+      />
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/restaurants/CreateCouponBatchScreen.js
+++ screens/restaurants/CreateCouponBatchScreen.js
@@ -1,36 +1,92 @@
 
 // This is how to upload an image to the AMAZON S3 server I've set up with Nima 
-import { accessKeyId, secretAccessKey } from 'react-native-dotenv'
-ApiClient.init(accessKeyId, secretAccessKey)
+// import { accessKeyId, secretAccessKey } from 'react-native-dotenv'
+// ApiClient.init(accessKeyId, secretAccessKey)
 
-const AWS = require('aws-sdk');
-const fs = require('fs');
-const path = require('path');
+// const AWS = require('aws-sdk');
+// const fs = require('fs');
+// const path = require('path');
 
-//configuring the AWS environment
-AWS.config.update({
-    accessKeyId: accessKeyId,
-    secretAccessKey: secretAccessKey
-  });
+// //configuring the AWS environment
+// AWS.config.update({
+//     accessKeyId: accessKeyId,
+//     secretAccessKey: secretAccessKey
+//   });
 
-var s3 = new AWS.S3();
-var filePath = "Headshot.jpg";
+// const s3 = new AWS.S3();
 
-//configuring parameters
-var params = {
-  Bucket: 'eatee',
-  Body : fs.createReadStream(filePath),
-  Key : "Images/"+Date.now()+"_"+path.basename(filePath)
-};
+// let filePath = "Headshot.jpg";
 
-s3.upload(params, function (err, data) {
-  //handle error
-  if (err) {
-    console.log("Error", err);
+// //configuring parameters
+// const params = {
+//   Bucket: 'eatee',
+//   Body : fs.createReadStream(filePath),
+//   Key : "Images/"+Date.now()+"_"+path.basename(filePath)
+// };
+
+// s3.upload(params, function (err, data) {
+//   //handle error
+//   if (err) {
+//     console.log("Error", err);
+//   }
+
+//   //success
+//   if (data) {
+//     console.log("Uploaded in:", data.Location);
+//   }
+// });
+
+
+import React from 'react';
+import { Text, View, Button, TextInput } from "react-native";
+import axios from 'axios';
+import { ImagePicker } from 'expo';
+
+export default class CreateCouponBatchScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {},
+      image: null,
+      
+    };
   }
 
-  //success
-  if (data) {
-    console.log("Uploaded in:", data.Location);
+  static navigationOptions = {
+    title: 'Create Your Ad Here',
+  };
+
+  componentDidMount(){
+    console.log("Component Mounted")
   }
-});
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
+  };
+
+  // Save IMAGE, DISH NAME, DESC, TIMER, QUANTITY
+  render() {
+    let { image } = this.state;
+
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          title="Pick an image from camera roll"
+          onPress={this._pickImage}
+        />
+        {image &&
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        
+      </View>
+    )
+  }
+}
