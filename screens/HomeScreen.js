25d896dbf18c16b7eafcf94b5087ff9780c91451
Hoa Hoang <6288799+tienhoah@users.noreply.github.com>
2018-09-27 11:06:24 -0700

    change HomeScreen background color.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -51,7 +51,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'white'
+    backgroundColor: 'tomato'
   },
   button: {
     width: 210,
