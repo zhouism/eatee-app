4e442a81373b7f7e3f8b265f85fe8ec5cc0307f2
Hoa Hoang <6288799+tienhoah@users.noreply.github.com>
2018-09-27 11:33:13 -0700

    Oofs

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -29,9 +29,10 @@ doFacebookLogin = async dispatch => {
     return dispatch({ type: FACEBOOK_LOGIN_FAIL })
   } else if (type === 'success') {
     const response = await fetch(
-      `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email`);
+      `https://graph.facebook.com/me?access_token=${token}&fields=id,first_name,last_name,email`);
       const userInfo = await response.json();
-      console.log(userInfo.name);
+      console.log(userInfo.first_name);
+      console.log(userInfo.last_name);
   }
 
   await AsyncStorage.setItem('fb_token', token);
