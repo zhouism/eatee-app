95bb2143008d086fbf6857df45a30d3891ecdf0b
Hoa Hoang <6288799+tienhoah@users.noreply.github.com>
2018-09-28 08:24:55 -0700

    debug with console.log, this branch is working for logging in

--- actions/coupons_actions.js
+++ actions/coupons_actions.js
@@ -8,6 +8,7 @@ export const fetchCouponBatches = () => async dispatch => {
     try {
         console.log('fetching...');
         let response = await axios.get('http://192.168.1.97:3001/api/coupon_batches');
+        console.log('response from fetch: ', response.data);
         dispatch({ type: FETCH_COUPON_BATCHES, payload: response.data });
     } catch(e) {
         console.error(e);
