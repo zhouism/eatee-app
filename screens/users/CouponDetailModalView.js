043c759d8e111b701248c7485ae3502097e3c3df
veilance <christopher.tam@alumni.ubc.ca>
2018-09-27 18:25:17 -0700

    User is able to redeem a coupon only once and when redeemed the UI will display text to user that the coupon has been redeemed

--- screens/users/CouponDetailModalView.js
+++ screens/users/CouponDetailModalView.js
@@ -9,20 +9,35 @@ import {
   TouchableHighlight,
   Image
 } from "react-native";
+import axios from 'axios';
 
 export default class ModalView extends React.Component {
   constructor() {
     super();
     this.state = {
       modalVisible: false,
-      result: {}
+      item: {},
+      is_redeemed: false
     };
   }
 
   componentWillReceiveProps(nextProps) {
     this.setState({
       modalVisible: nextProps.modalVisible,
-      result: nextProps.result
+      item: nextProps.item,
+      is_redeemed: nextProps.item['is_redeemed']
+    });
+  }
+
+  _redeemCoupon() { 
+    axios.post(`http://192.168.0.191:3001/api/coupon_details/${this.state.item.id}`)
+    .then(() =>  {
+      this.setState({
+        is_redeemed: true
+      })
+    })
+    .catch(function (error) {
+      console.log(error);
     });
   }
 
@@ -34,18 +49,19 @@ export default class ModalView extends React.Component {
         visible={this.state.modalVisible}
         onRequestClose={() => {
           this.props.setModalVisible(false);
+          this.props.updateDB()
         }}
       >
         <View>
           <View>
-          <Image source={{uri: this.state.result.image}} style={{width: 400, height: 300}} />
-            <Text>Id: {this.state.result.id}</Text>
-            <Text>Restaurant: {this.state.result.name}</Text>
-            <Text>Restaurants Address: {this.state.result.address}</Text>
-            <Text>Dish Name: {this.state.result.dish_name}</Text>
-            <Text>Time Limit: {this.state.result.time_limit}</Text>
-            <Text>Unit Price: ${(this.state.result.price * 1).toFixed(2)}</Text>
-            <Text>Your Price: ${(this.state.result.price * (this.state.result.discount / 100)).toFixed(2)} </Text>
+          <Image source={{uri: this.state.item.image}} style={{width: 400, height: 300}} />
+            <Text>Id: {this.state.item.id}</Text>
+            <Text>Restaurant: {this.state.item.name}</Text>
+            <Text>Restaurants Address: {this.state.item.address}</Text>
+            <Text>Dish Name: {this.state.item.dish_name}</Text>
+            <Text>Time Limit: {this.state.item.time_limit}</Text>
+            <Text>Unit Price: ${(this.state.item.price * 1).toFixed(2)}</Text>
+            <Text>Your Price: ${(this.state.item.price * (this.state.item.discount / 100)).toFixed(2)} </Text>
             <TouchableHighlight
               onPress={() => {
                 this.props.setModalVisible(false);
@@ -53,6 +69,8 @@ export default class ModalView extends React.Component {
             >
               <Text>Hide Modal</Text>
             </TouchableHighlight>
+            {this.state.is_redeemed ? 
+            <Text>Your Coupon Has Been Redeemed</Text> : <Button onPress={() => this._redeemCoupon()} title="Redeem Coupon"/> }
           </View>
         </View>
       </Modal>
