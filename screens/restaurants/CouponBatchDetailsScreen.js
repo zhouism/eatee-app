20622d960258bb8d3e281db047351fc46655a192
Hoa Hoang <6288799+tienhoah@users.noreply.github.com>
2018-10-01 11:44:35 -0700

    all metrics are now rendered for each coupon batch with number of impressions, and swipe total.

--- screens/restaurants/CouponBatchDetailsScreen.js
+++ screens/restaurants/CouponBatchDetailsScreen.js
@@ -11,13 +11,35 @@ import {
   Image
 } from "react-native";
 import axios from 'axios';
+import { rootIP } from "react-native-dotenv";
 
 export default class ModalView extends React.Component {
   constructor() {
     super();
     this.state = {
-      item: {},
+      couponSwipe: ''
     };
+  }
+
+  componentDidMount() {
+    const newItem = this.props.navigation.getParam('item')
+    this.getCouponSwipe(newItem.id)
+  }
+
+  getCouponSwipe(coupon) {
+   axios
+    .get(`http://${rootIP}:3001/api/coupon_batches/${coupon}/swipe`)
+    .then(response => {
+
+      console.log('state change')
+      console.log('response', (response.data)[0].count)
+      this.setState({
+        couponSwipe: (response.data)[0].count
+      })
+    }).catch(error => {
+      console.log(error);
+    });
+
   }
 
   render() {
@@ -29,11 +51,15 @@ export default class ModalView extends React.Component {
       <View>
           <View>
           <Image source={{uri: item.image}} style={{width: 400, height: 300}} />
+            <Text>{this.props.navigation.item}</Text>
             <Text>Id: {item.id}</Text>
             <Text>Dish Name: {item.dish_name}</Text>
             <Text>Time Limit: {item.time_limit}</Text>
             <Text>Unit Price: ${(item.price * 1).toFixed(2)}</Text>
-            <Text>Your Price: ${(item.price * (item.discount / 100)).toFixed(2)} </Text>    
+            <Text>Your Price: ${(item.price * (item.discount / 100)).toFixed(2)} </Text>
+            <Text>Impression: {item.impression}</Text>
+            <Text>Total Swipe: {this.state.couponSwipe}</Text>
+
           </View>
         </View>
       </ScrollView>
