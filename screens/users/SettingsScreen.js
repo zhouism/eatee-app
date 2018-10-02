0840eee922fb9ae16db21abd0f0f6b6f1546e201
Julia Zhou <juliazhouism@gmail.com>
2018-10-02 16:06:27 -0700

    user settings page styled

--- screens/users/SettingsScreen.js
+++ screens/users/SettingsScreen.js
@@ -4,7 +4,6 @@ import { Button, Slider } from "react-native-elements";
 import { connect } from "react-redux";
 import * as actions from "../../actions";
 
-
 class SettingsScreen extends React.Component {
   static navigationOptions = ({ navigation }) => {
     return {
@@ -14,29 +13,19 @@ class SettingsScreen extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      value: 5
-    };
   }
-
 
   _handleLogout() {
     this.props.facebookLogout();
-    this.props.navigation.navigate('Home')
+    this.props.navigation.navigate("Home");
   }
 
   render() {
     return (
-      <View>
-        <Text>USER LOCATION SETTINGS</Text>
-        <Text>USER PRICE SETTINGS</Text>
-        <Slider
-    value={this.state.value}
-    onValueChange={(value) => this.setState({value})} />
-    <Text>Value: {this.state.value}</Text>
+      <View style={styles.container}>
         <Button
           large
-          icon={{ name: "sign-out", type: "font-awesome" }}
+          buttonStyle={styles.button}
           title="LOGOUT"
           onPress={() => this._handleLogout()}
         />
@@ -46,7 +35,7 @@ class SettingsScreen extends React.Component {
 }
 
 function mapStateToProps({ auth }) {
-  console.log('user logged out. Current user token:', auth.token);
+  console.log("user logged out. Current user token:", auth.token);
   return { token: auth.token };
 }
 
@@ -54,3 +43,20 @@ export default connect(
   mapStateToProps,
   actions
 )(SettingsScreen);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#FC4E3E"
+  },
+  button: {
+    backgroundColor: "#000000",
+    marginTop: 10,
+    width: 300,
+    borderRadius: 30,
+    margin: 10,
+    borderColor: "#FC4E3E"
+  }
+});
